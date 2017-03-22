93d172929fa974d51a34de15c698de3b0b282da9
Manolis Surligas <surligas@gmail.com>
2017-03-23 01:10:41 +0200

    Fix missing library versioning on CMake

--- apps/flowgraphs/satellites/upsat_transceiver_qt.py
+++ apps/flowgraphs/satellites/upsat_transceiver_qt.py
@@ -5,7 +5,7 @@
 # Title: UPSat Transceiver QT
 # Author: Manolis Surligas (surligas@gmail.com)
 # Description: SATNOGS transceiver for UPSAT satellite
-# Generated: Sat Feb  4 21:55:55 2017
+# Generated: Sun Feb  5 13:59:36 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -41,7 +41,7 @@ import time
 
 class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
 
-    def __init__(self, recv_port=16886, send_port=5022, wod_port=5023, lo_offset=100e3, bind_addr='0.0.0.0', rx_sdr_device='usrpb200', dest_addr='127.0.0.1', tx_sdr_device='usrpb200'):
+    def __init__(self, bind_addr='0.0.0.0', dest_addr='127.0.0.1', lo_offset=100e3, recv_port=16886, rx_sdr_device='usrpb200', send_port=5022, tx_sdr_device='usrpb200', wod_port=5023):
         gr.top_block.__init__(self, "UPSat Transceiver QT")
         Qt.QWidget.__init__(self)
         self.setWindowTitle("UPSat Transceiver QT")
@@ -67,14 +67,14 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         ##################################################
         # Parameters
         ##################################################
-        self.recv_port = recv_port
-        self.send_port = send_port
-        self.wod_port = wod_port
-        self.lo_offset = lo_offset
         self.bind_addr = bind_addr
-        self.rx_sdr_device = rx_sdr_device
         self.dest_addr = dest_addr
+        self.lo_offset = lo_offset
+        self.recv_port = recv_port
+        self.rx_sdr_device = rx_sdr_device
+        self.send_port = send_port
         self.tx_sdr_device = tx_sdr_device
+        self.wod_port = wod_port
 
         ##################################################
         # Variables
@@ -108,6 +108,42 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.satnogs_qb50_deframer_0 = satnogs.qb50_deframer(0xe)
         self.satnogs_multi_format_msg_sink_0 = satnogs.multi_format_msg_sink(1)
         self.satnogs_ax25_decoder_bm_0 = satnogs.ax25_decoder_bm('GND', 0, False, True, 256, 3)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	50e3, #bw
+        	"", #name
+                1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+        
+        if not True:
+          self.qtgui_waterfall_sink_x_0.disable_legend()
+        
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
+        
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+        
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -206,6 +242,7 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.connect((self.digital_clock_recovery_mm_xx_0, 0), (self.digital_binary_slicer_fb_0, 0))    
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_quadrature_demod_cf_0_0, 0))    
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.qtgui_waterfall_sink_x_0, 0))    
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.analog_frequency_modulator_fc_0, 0))    
         self.connect((self.osmosdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))    
         self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.blocks_multiply_xx_0, 0))    
@@ -216,23 +253,17 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
-    def get_recv_port(self):
-        return self.recv_port
+    def get_bind_addr(self):
+        return self.bind_addr
 
-    def set_recv_port(self, recv_port):
-        self.recv_port = recv_port
+    def set_bind_addr(self, bind_addr):
+        self.bind_addr = bind_addr
 
-    def get_send_port(self):
-        return self.send_port
+    def get_dest_addr(self):
+        return self.dest_addr
 
-    def set_send_port(self, send_port):
-        self.send_port = send_port
-
-    def get_wod_port(self):
-        return self.wod_port
-
-    def set_wod_port(self, wod_port):
-        self.wod_port = wod_port
+    def set_dest_addr(self, dest_addr):
+        self.dest_addr = dest_addr
 
     def get_lo_offset(self):
         return self.lo_offset
@@ -244,11 +275,11 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.freq_xlating_fir_filter_xxx_0.set_center_freq(self.lo_offset)
         self.analog_sig_source_x_0.set_frequency(self.lo_offset )
 
-    def get_bind_addr(self):
-        return self.bind_addr
+    def get_recv_port(self):
+        return self.recv_port
 
-    def set_bind_addr(self, bind_addr):
-        self.bind_addr = bind_addr
+    def set_recv_port(self, recv_port):
+        self.recv_port = recv_port
 
     def get_rx_sdr_device(self):
         return self.rx_sdr_device
@@ -262,11 +293,11 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_bb_gain(satnogs.hw_rx_settings[self.rx_sdr_device]['bb_gain'], 0)
         self.osmosdr_source_0.set_antenna(satnogs.hw_rx_settings[self.rx_sdr_device]['antenna'], 0)
 
-    def get_dest_addr(self):
-        return self.dest_addr
+    def get_send_port(self):
+        return self.send_port
 
-    def set_dest_addr(self, dest_addr):
-        self.dest_addr = dest_addr
+    def set_send_port(self, send_port):
+        self.send_port = send_port
 
     def get_tx_sdr_device(self):
         return self.tx_sdr_device
@@ -277,6 +308,12 @@ class upsat_transceiver_qt(gr.top_block, Qt.QWidget):
         self.osmosdr_sink_0.set_if_gain(satnogs.hw_tx_settings[self.tx_sdr_device]['if_gain'], 0)
         self.osmosdr_sink_0.set_bb_gain(satnogs.hw_tx_settings[self.tx_sdr_device]['bb_gain'], 0)
         self.osmosdr_sink_0.set_antenna(satnogs.hw_tx_settings[self.tx_sdr_device]['antenna'], 0)
+
+    def get_wod_port(self):
+        return self.wod_port
+
+    def set_wod_port(self, wod_port):
+        self.wod_port = wod_port
 
     def get_samples_per_symbol_tx(self):
         return self.samples_per_symbol_tx
@@ -410,29 +447,29 @@ def argument_parser():
     description = 'SATNOGS transceiver for UPSAT satellite'
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
     parser.add_option(
-        "", "--recv-port", dest="recv_port", type="intx", default=16886,
-        help="Set recv_port [default=%default]")
-    parser.add_option(
-        "", "--send-port", dest="send_port", type="intx", default=5022,
-        help="Set send_port [default=%default]")
-    parser.add_option(
-        "", "--wod-port", dest="wod_port", type="intx", default=5023,
-        help="Set wod_port [default=%default]")
-    parser.add_option(
-        "", "--lo-offset", dest="lo_offset", type="eng_float", default=eng_notation.num_to_str(100e3),
-        help="Set lo_offset [default=%default]")
-    parser.add_option(
         "", "--bind-addr", dest="bind_addr", type="string", default='0.0.0.0',
         help="Set bind_addr [default=%default]")
-    parser.add_option(
-        "", "--rx-sdr-device", dest="rx_sdr_device", type="string", default='usrpb200',
-        help="Set rx_sdr_device [default=%default]")
     parser.add_option(
         "", "--dest-addr", dest="dest_addr", type="string", default='127.0.0.1',
         help="Set dest_addr [default=%default]")
     parser.add_option(
+        "", "--lo-offset", dest="lo_offset", type="eng_float", default=eng_notation.num_to_str(100e3),
+        help="Set lo_offset [default=%default]")
+    parser.add_option(
+        "", "--recv-port", dest="recv_port", type="intx", default=16886,
+        help="Set recv_port [default=%default]")
+    parser.add_option(
+        "", "--rx-sdr-device", dest="rx_sdr_device", type="string", default='usrpb200',
+        help="Set rx_sdr_device [default=%default]")
+    parser.add_option(
+        "", "--send-port", dest="send_port", type="intx", default=5022,
+        help="Set send_port [default=%default]")
+    parser.add_option(
         "", "--tx-sdr-device", dest="tx_sdr_device", type="string", default='usrpb200',
         help="Set tx_sdr_device [default=%default]")
+    parser.add_option(
+        "", "--wod-port", dest="wod_port", type="intx", default=5023,
+        help="Set wod_port [default=%default]")
     return parser
 
 
@@ -446,7 +483,7 @@ def main(top_block_cls=upsat_transceiver_qt, options=None):
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls(recv_port=options.recv_port, send_port=options.send_port, wod_port=options.wod_port, lo_offset=options.lo_offset, bind_addr=options.bind_addr, rx_sdr_device=options.rx_sdr_device, dest_addr=options.dest_addr, tx_sdr_device=options.tx_sdr_device)
+    tb = top_block_cls(bind_addr=options.bind_addr, dest_addr=options.dest_addr, lo_offset=options.lo_offset, recv_port=options.recv_port, rx_sdr_device=options.rx_sdr_device, send_port=options.send_port, tx_sdr_device=options.tx_sdr_device, wod_port=options.wod_port)
     tb.start()
     tb.show()
 
